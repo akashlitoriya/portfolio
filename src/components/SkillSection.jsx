be26ef481a35754e1e56408d25c7a06106936245
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-11-03 15:13:00 +0530

    Skill section to animate only once

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -25,18 +25,18 @@ const SkillSection = () => {
     <div className='min-w-full w-full h-fit sm:h-96 bg-primary pt-8'>
         <div className='max-w-maxContent h-full bgVector w-full flex flex-col justify-center items-center mx-auto p-6'>
             <div className='flex flex-col gap-5 w-full'>
-                <motion.h1 initial={{opacity: 0, translateX:-200}} whileInView={{opacity: 1, translateX: 0}} transition={{duration: 1}} className='block sm:hidden font-robo text-2xl font-semibold text-secondary-50'>Skills</motion.h1>
+                <motion.h1 viewport={{ once: true}} initial={{opacity: 0, translateX:-200}} whileInView={{opacity: 1, translateX: 0}} transition={{duration: 1}} className='block sm:hidden font-robo text-2xl font-semibold text-secondary-50'>Skills</motion.h1>
                 <motion.div  viewport={{ once: true}}  className='w-full h-full flex flex-col gap-6 sm:flex-row sm:flex-wrap items-center justify-between'>
-                    <motion.div initial="offscreen" whileInView="onscreen" variants={cardVariants}  className='border-secondary-50 rounded-lg min-w-[230px] sm:min-w-[200px] min-h-[100px] flex justify-center items-center px-4 py-2 sm:w-fit h-fit bg-richgray-700 border-[3px] '>
+                    <motion.div initial="offscreen" whileInView="onscreen" variants={cardVariants} viewport={{ once: true}}  className='border-secondary-50 rounded-lg min-w-[230px] sm:min-w-[200px] min-h-[100px] flex justify-center items-center px-4 py-2 sm:w-fit h-fit bg-richgray-700 border-[3px] '>
                         <img src={react} alt='react-icon' />
                     </motion.div>
-                    <motion.div initial="offscreen" whileInView="onscreen" variants={cardVariants} className='border-secondary-50 rounded-lg min-w-[230px] sm:min-w-[200px] min-h-[100px] flex justify-center items-center px-4 py-2 sm:w-fit h-fit bg-richgray-700 border-[3px] '>
+                    <motion.div initial="offscreen" whileInView="onscreen" variants={cardVariants} viewport={{ once: true}} className='border-secondary-50 rounded-lg min-w-[230px] sm:min-w-[200px] min-h-[100px] flex justify-center items-center px-4 py-2 sm:w-fit h-fit bg-richgray-700 border-[3px] '>
                         <img src={nodejs} alt='nodejs-icon' />
                     </motion.div>
-                    <motion.div initial="offscreen" whileInView="onscreen" variants={cardVariants} className='border-secondary-50 rounded-lg min-w-[230px] sm:min-w-[200px] min-h-[100px] flex justify-center items-center px-4 py-2 sm:w-fit h-fit bg-richgray-700 border-[3px] ' >
+                    <motion.div initial="offscreen" whileInView="onscreen" variants={cardVariants} viewport={{ once: true}} className='border-secondary-50 rounded-lg min-w-[230px] sm:min-w-[200px] min-h-[100px] flex justify-center items-center px-4 py-2 sm:w-fit h-fit bg-richgray-700 border-[3px] ' >
                         <img src={express} alt='express-icon' />
                     </motion.div>
-                    <motion.div initial="offscreen" whileInView="onscreen" variants={cardVariants} className='border-secondary-50 rounded-lg min-w-[230px] sm:min-w-[200px] min-h-[100px] flex justify-center items-center px-4 py-2 sm:w-fit h-fit bg-richgray-700 border-[3px] '>
+                    <motion.div initial="offscreen" whileInView="onscreen" variants={cardVariants} viewport={{ once: true}} className='border-secondary-50 rounded-lg min-w-[230px] sm:min-w-[200px] min-h-[100px] flex justify-center items-center px-4 py-2 sm:w-fit h-fit bg-richgray-700 border-[3px] '>
                         <img src={mongodb} alt='mongodb-icon' />
                     </motion.div>
 
