8e7374793dba786456c8e108f840423b3410d6fb
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-11-04 13:29:34 +0530

    Project Modal created

--- src/data/Project.js
+++ src/data/Project.js
@@ -3,18 +3,41 @@ export const project = [
         title: "StudyNotion",
         description: "StudyNotion is a robust LMS platform, powered by the MERN stack, offering an intuitive and efficient e-learning experience.",
         thumbnail: "https://res.cloudinary.com/dvu9lo8mq/image/upload/v1698955542/Portfolio/xldpel4crv3ylkl3xumo.png",
-        link: "https://study-notion-akash-litoriya.vercel.app/"
+        link: "https://study-notion-akash-litoriya.vercel.app/",
+        gitHub: "https://github.com/akashlitoriya/StudyNotion",
+        skills:[
+            "ReactJS",
+            "NodeJS",
+            "ExpressJS",
+            "MongoDB",
+            "Redux",
+            "TailwindCSS",
+        ]
     },
     {
         title: "FoodVilla",
         description: "A sleek ReactJS and TailwindCSS powered front-end app for seamless food ordering, offering a delightful user experience.",
         thumbnail: "https://res.cloudinary.com/dvu9lo8mq/image/upload/v1698955465/Portfolio/ijpkhztkvz2lbpi5gxte.png",
-        link: "https://foodvillaakash.netlify.app/"
+        link: "https://foodvillaakash.netlify.app/",
+        gitHub: "https://github.com/akashlitoriya/NamasteReact",
+        skills:[
+            "ReactJS",
+            "TailwindCSS",
+            "Redux",
+            "Babel",
+        ]
+        
     },
     {
         title: "Utero",
         description: "A sleek, responsive static website for online shopping, meticulously crafted with HTML, CSS, and JavaScript for a seamless user experience.",
         thumbnail: "https://res.cloudinary.com/dvu9lo8mq/image/upload/v1698955635/Portfolio/iqzrs3fc6kdomhpjq6yp.png",
-        link: "https://akashlitoriya.github.io/ShoppingWebsite.github.io/"
+        link: "https://akashlitoriya.github.io/ShoppingWebsite.github.io/",
+        gitHub: "https://github.com/akashlitoriya/ShoppingWebsite.github.io",
+        skills:[
+            "HTML",
+            "CSS",
+            "JavaScript",
+        ]
     }
 ]
